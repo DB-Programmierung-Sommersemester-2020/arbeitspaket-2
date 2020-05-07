c0393e79ddac1fdf0585dfe2627ce2bf81f6ac2f
VChouliak <chouliak@hotmail.com>
2020-05-08 01:44:38 +0200

    Adjust signature of getBooksByAuthor

--- buchlager/src/main/java/buchlager/repositories/services/BookRepositoryService.java
+++ buchlager/src/main/java/buchlager/repositories/services/BookRepositoryService.java
@@ -2,11 +2,12 @@ package buchlager.repositories.services;
 
 import java.util.Set;
 
+import buchlager.models.Author;
 import buchlager.models.Book;
 
 public interface BookRepositoryService extends Repository<Book, Integer> {
     Set<Book> getBooksByTitle(String title);
-    Set<Book> getBooksByAuthor(String author);
+    Set<Book> getBooksByAuthor(Author author);
     Set<Book> getBooksByPublischer(String publischer);
 
 }
