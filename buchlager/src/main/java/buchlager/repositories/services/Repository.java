edbfcf80c39da0ad5a8b643bf382cb1f251f5e48
VChouliak <chouliak@hotmail.com>
2020-05-07 22:10:28 +0200

    Adjust signature

--- buchlager/src/main/java/buchlager/repositories/services/Repository.java
+++ buchlager/src/main/java/buchlager/repositories/services/Repository.java
@@ -5,9 +5,9 @@ import java.util.Collection;
 public interface Repository<T, K> {
     T getById(K id);
     
-    boolean create();
-    boolean update();
-    boolean delete();
+    boolean create(T entity);
+    boolean update(T entity);
+    boolean delete(T entity);
 
     Collection<T> getAll();
 }
