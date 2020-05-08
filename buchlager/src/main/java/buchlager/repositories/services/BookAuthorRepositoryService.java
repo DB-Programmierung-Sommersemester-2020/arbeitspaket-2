158dc2338e2f92cbd42699f4e06fa45721012dd5
VChouliak <chouliak@hotmail.com>
2020-05-08 18:56:37 +0200

    Adjust methods signature

--- buchlager/src/main/java/buchlager/repositories/services/BookAuthorRepositoryService.java
+++ buchlager/src/main/java/buchlager/repositories/services/BookAuthorRepositoryService.java
@@ -2,12 +2,15 @@ package buchlager.repositories.services;
 
 import java.util.List;
 
+import buchlager.models.BookAuthor;
+
 public interface BookAuthorRepositoryService {
     List<Integer> findBooksOfAuthor(int authorId);
-    List<Integer> findAuthorsOfBook();
-
+    List<Integer> findAuthorsOfBook(int bookId);
+    List<BookAuthor> getAllReferences();
+    BookAuthor getById(int id);
     boolean createBookAutorReference(int buchId, int autorId);
-    boolean updateBookAutorReference(int buchId, int autorId);
+    boolean updateBookAutorReference(BookAuthor bookAuthor);
     boolean deleteBookAutorReference(int buchId, int autorId);
     boolean deleteBuchReferenz(int buchId);
     boolean deleteAutorReferenz(int autorId);
