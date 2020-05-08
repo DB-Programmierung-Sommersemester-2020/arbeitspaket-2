ed0803efbabe0c0e3da49b3915ab93a71c826fa3
VChouliak <chouliak@hotmail.com>
2020-05-08 15:53:31 +0200

    Adjust Signature

--- buchlager/src/main/java/buchlager/repositories/services/PublisherRepositoryService.java
+++ buchlager/src/main/java/buchlager/repositories/services/PublisherRepositoryService.java
@@ -3,11 +3,12 @@ package buchlager.repositories.services;
 import java.util.Set;
 
 import buchlager.models.Address;
+import buchlager.models.Book;
 import buchlager.models.Publisher;
 
 
-public interface PublisherRepositoryService {
+public interface PublisherRepositoryService extends Repository<Publisher,Integer>{
     Set<Publisher> getPublischerByName(String name);
-    Set<Publisher> getPublisherByBook(Publisher publisher);
+    Set<Publisher> getPublisherByBook(Book publisher);
     Set<Publisher> getPublisherByAddress(Address address);
 }
